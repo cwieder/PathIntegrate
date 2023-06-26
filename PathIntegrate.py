fe912d42f690acfcb5c73e52dbd5718c369aefd5
cwieder <cwieder09@gmail.com>
2023-06-26 16:30:14 +0100

    add main script

---
+++ PathIntegrate.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import sspa
+import sklearn
+from mbpls.mbpls import MBPLS
+
+class PathIntegrate:
+
+    def __init__(self, omics_data:dict, metadata, pathway_source, sspa_scoring='svd'):
+        self.omics_data = omics_data
+        self.metadata = metadata
+        self.pathway_source = pathway_source
+        self.sspa_scoring = sspa_scoring
+        
+        sspa_methods = {'svd': sspa.sspa_svd, 'ssGSEA': sspa.sspa_ssGSEA, 'kpca': sspa.sspa_kpca, 'ssClustPA': sspa.sspa_ssClustPA, 'zscore': sspa.sspa_zscore}
+        self.sspa_method = sspa_methods[self.sspa_scoring]
+
+        self.labels = pd.factorize(self.metadata)[0]
+
+    def MultiView(self, ncomp=2):
+        sspa_scores = [self.sspa_method(i, self.pathway_source) for i in self.omics_data.values()]
+        mv = MBPLS(n_components=ncomp)
+        mv.fit(sspa_scores, self.labels)
+
+        return mv
+
+    def SingleView(self):
+        pass
+
+
+metab = pd.read_csv('metabolomics_example.csv', index_col=0)
+prot = pd.read_csv('proteomics_example.csv', index_col=0)
+
+# make possible to download MO paths from reactome
+mo_paths = pd.read_csv("../Pathway_databases/Reactome_multi_omics_ChEBI_Uniprot.csv", index_col=0)
+
+pi_model  = PathIntegrate({'Metabolomics': metab, 'Proteomics':prot.iloc[:, :-1]}, metadata=prot['Group'], pathway_source=mo_paths, sspa_scoring='zscore')
+
+covid_multi_view = pi_model.MultiView(ncomp=5)
+print(covid_multi_view)
