8a7264626e97643708d4c0f58df1e4f963d064b1
cwieder <cwieder09@gmail.com>
2023-10-09 21:26:24 +0100

    update docstrings

--- src/pathintegrate/pathintegrate.py
+++ src/pathintegrate/pathintegrate.py
@@ -9,6 +9,31 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import cross_val_score, GridSearchCV
 
 class PathIntegrate:
+    '''PathIntegrate class for multi-omics pathway integration.
+
+    Args:
+        omics_data (dict): Dictionary of omics data. Keys are omics names, values are pandas DataFrames containing omics data where rows contain samples and columns reprsent features.
+        metadata (pandas.Series): Metadata for samples. Index is sample names, values are class labels.
+        pathway_source (pandas.DataFrame): GMT style pathway source data. Must contain column 'Pathway_name'. 
+        sspa_scoring (object, optional): Scoring method for ssPA. Defaults to sspa.sspa_SVD. Options are sspa.sspa_SVD, sspa.sspa_ssGSEA, sspa.sspa_KPCA, sspa.sspa_ssClustPA, sspa.sspa_zscore.
+        min_coverage (int, optional): Minimum number of molecules required in a pathway. Defaults to 3.
+
+    Attributes:
+        omics_data (dict): Dictionary of omics data. Keys are omics names, values are pandas DataFrames.
+        omics_data_scaled (dict): Dictionary of omics data scaled to mean 0 and unit variance. Keys are omics names, values are pandas DataFrames.
+        metadata (pandas.Series): Metadata for samples. Index is sample names, values are class labels.
+        pathway_source (pandas.DataFrame): Pathway source data.
+        pathway_dict (dict): Dictionary of pathways. Keys are pathway names, values are lists of molecules.
+        sspa_scoring (object): Scoring method for SSPA.
+        min_coverage (int): Minimum number of omics required to cover a pathway.
+        sspa_method (object): SSPA scoring method.
+        sspa_scores_mv (dict): Dictionary of SSPA scores for each omics data. Keys are omics names, values are pandas DataFrames.
+        sspa_scores_sv (pandas.DataFrame): SSPA scores for all omics data concatenated.
+        coverage (dict): Dictionary of pathway coverage. Keys are pathway names, values are number of omics covering the pathway.
+        mv (object): Fitted MultiView model.
+        sv (object): Fitted SingleView model.
+        labels (pandas.Series): Class labels for samples. Index is sample names, values are class labels.
+    '''
 
     def __init__(self, omics_data:dict, metadata, pathway_source, sspa_scoring=sspa.sspa_SVD, min_coverage=3):
         self.omics_data = omics_data
@@ -36,6 +61,15 @@ class PathIntegrate:
         return coverage
 
     def MultiView(self, ncomp=2):
+        """Fits a PathIntegrate MultiView model using MBPLS.
+
+        Args:
+            ncomp (int, optional): Number of components. Defaults to 2.
+
+        Returns:
+            object: Fitted PathIntegrate MultiView model.
+        """
+
         print('Generating pathway scores...')
         sspa_scores_ = [self.sspa_method(self.pathway_source, self.min_coverage) for i in self.omics_data_scaled.values()]
         sspa_scores = [sspa_scores_[n].fit_transform(i) for n, i in enumerate(self.omics_data_scaled.values())]
@@ -78,6 +112,7 @@ class PathIntegrate:
         Returns:
             object: Fitted PathIntegrate SingleView model.
         """
+
         concat_data = pd.concat(self.omics_data_scaled.values(), axis=1)
         print('Generating pathway scores...')
 
@@ -104,6 +139,16 @@ class PathIntegrate:
     
     # cross-validation approaches
     def SingleViewCV(self, model=sklearn.linear_model.LogisticRegression, model_params=None, cv_params=None):
+        '''Cross-validation for SingleView model.
+        
+        Args:
+            model (object, optional): SKlearn prediction model class. Defaults to sklearn.linear_model.LogisticRegression.
+            model_params (_type_, optional): Model-specific hyperparameters. Defaults to None.
+            cv_params (dict, optional): Cross-validation parameters. Defaults to None.
+
+        Returns:
+            object: Cross-validation results.
+        '''
 
         # concatenate omics - unscaled to avoid data leakage
         concat_data = pd.concat(self.omics_data.values(), axis=1)
@@ -119,6 +164,17 @@ class PathIntegrate:
         return cv_res
     
     def SingleViewGridSearchCV(self, param_grid, model=sklearn.linear_model.LogisticRegression, grid_search_params=None):
+        '''Grid search cross-validation for SingleView model.
+        
+        Args:
+            param_grid (dict): Grid search parameters.
+            model (object, optional): SKlearn prediction model class. Defaults to sklearn.linear_model.LogisticRegression.
+            grid_search_params (dict, optional): Grid search parameters. Defaults to None.
+            
+        Returns:
+            object: GridSearchCV object.
+                
+        '''
         # concatenate omics - unscaled to avoid data leakage
         concat_data = pd.concat(self.omics_data.values(), axis=1)
 
@@ -135,6 +191,12 @@ class PathIntegrate:
         return grid_search
 
     def MultiViewCV(self):
+        '''Cross-validation for MultiView model.
+
+        Returns:
+            object: Cross-validation results.
+        '''
+
         # Set up sklearn pipeline
         pipe_mv = sklearn.pipeline.Pipeline([
             ('sspa', self.sspa_method(self.pathway_source, self.min_coverage)),
@@ -151,6 +213,17 @@ class PathIntegrate:
 
 
 def VIP_multiBlock(x_weights, x_superscores, x_loadings, y_loadings):
+    """Calculate VIP scores for multi-block PLS.
+
+    Args:
+        x_weights (list): List of x weights.
+        x_superscores (list): List of x superscores.
+        x_loadings (list): List of x loadings.
+        y_loadings (list): List of y loadings.
+
+    Returns:
+        numpy.ndarray: VIP scores for each feature across all blocks. Features are in original order.
+    """
     # stack the weights from all blocks 
     weights = np.vstack(x_weights)
     # calculate product of sum of squares of superscores and y loadings
